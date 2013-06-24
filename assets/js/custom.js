2a3aa055c38ef11008eed32296c84fddc2131740
Redfrost <contact@simonalex.com>
2013-06-25 04:10:21 +1000

    V0.1a

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -110,6 +110,13 @@ $(fitWindow);
 
 		
 // Add your own		
+    // Hide content until the page is fully loaded
+    $(document).ready(function() {
+        $(window).load(function() {
+          // When the page has loaded
+          $("body").fadeTo("slow", 1);
+        });
+    });
 		
 		
 		

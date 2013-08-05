2352a4e13472f1ef7dd5db4e4919c395423198fe
Redfrost <contact@simonalex.com>
2013-08-05 11:54:26 +1000

    Link test

--- demo/assets/js/custom.js
+++ demo/assets/js/custom.js
@@ -117,19 +117,3 @@ function fitWindow() {
 // Add your own		
 
 
-// Mobile app open link internally
-    $( document ).on(
-        "click","#footer a", function( event ){
-         
-        // Stop the default behavior of the browser, which
-        // is to change the URL of the page.
-        event.preventDefault();
-         
-        // Manually change the location of the page to stay in
-        // "Standalone" mode and change the URL at the same time.
-        location.href = $( event.target ).attr( "href" );
-         
-        }
-    );
-    
-    
